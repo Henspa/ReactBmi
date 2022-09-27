fa0a142c1ae67983575dc8f5ef12046db9564c35
Henspa <81181977+Henspa@users.noreply.github.com>
2022-09-27 19:18:45 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [bmi, setBmi] = useState(0);
+
+  function calculate() {
+    const heightWithDigit = height.replace(',', '.');
+    const weightWithDigit = weight.replace(',', '.');
+    const result = (weightWithDigit / Math.pow(heightWithDigit, 2).toFixed(1));
+    setBmi(result);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Height</Text>
+      <TextInput value={String(height)} onChangeText={text => setHeight(text)}
+        keyboard-type='decimal-pad'>
+      </TextInput>
+      <Text>Weight</Text>
+      <TextInput value={String(weight)} onChangeText={text => setWeight(text)}
+        keyboard-type='decimal-pad'>
+      </TextInput>
+      <Button onPress={calculate} title="Calculate"></Button>
+      <Text>BMI: {bmi}</Text>
     </View>
   );
 }
